d2ce968c7dd6f17bb757cdb9625815cd3d30270b
hemanthrajgandham <33430687+hemanthrajgandham@users.noreply.github.com>
2026-06-17 20:49:09 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform{
 backend "s3"{
     bucket="zen-pharma-terraform-state-hemanthrajgandham"
-    key="envs/${terraform.workspace}/terraform.tfstate"
+    key="terraform.tfstate"
     encrypt=true
     use_lockfile=true
     region="us-east-1"
