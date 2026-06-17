e6f870c4c389c22ecd3b1970cff5bb2426e147c3
hemanthrajgandham <33430687+hemanthrajgandham@users.noreply.github.com>
2026-06-17 20:36:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ terraform{
       version="~>5.0"
     }
   }
-  backend "S3"{
+  backend "s3"{
     bucket="zen-pharma-terraform-state-hemanthrajgandham"
     key="envs/${terraform.workspace}/terraform.tfstate"
     encrypt=true
