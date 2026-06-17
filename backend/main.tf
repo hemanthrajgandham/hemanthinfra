a3adf45e3fe61688d6562e14f3796f6bc24e329c
hemanth <5hemanthunplugged@gmail.com>
2026-06-17 20:31:42 +0530

    adding new files

---
+++ backend/main.tf
@@ -0,0 +1,52 @@
+terraform{
+    required_providers{
+        aws={
+            source="hashicorp/aws"
+            version="~>5.0"
+        }
+    }
+}
+
+provider "aws"{
+    region="us-east-1"
+}
+# Create the bucket
+
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "zen-pharma-terraform-state-hemanthrajgandham"
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "shared"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
+
--- provider.tf
+++ provider.tf
@@ -1,9 +1,18 @@
 terraform{
+
   required_providers{
     aws={
       source="hashicorp/aws"
       version="~>5.0"
     }
+  }
+  backend "S3"{
+    bucket="zen-pharma-terraform-state-hemanthrajgandham"
+    key="envs/${terraform.workspace}/terraform.tfstate"
+    encrypt=true
+    use_lockfile=true
+    region="us-east-1"
+  
   }
 }
 
