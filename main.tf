be32eac40f307d1a0c05a96259781326befbbe53
hemanth <5hemanthunplugged@gmail.com>
2026-06-18 15:10:16 +0530

    adding eks module

--- main.tf
+++ main.tf
@@ -10,12 +10,21 @@ locals{
 module "vpc"{
     source="./modules/vpc"
     vpccidr="10.0.0.0/16"
-    projectname=local.defaulttags.project
-    environment=local.defaulttags.environment
     publicsubnetcidr=["10.0.1.0/24", "10.0.2.0/24"]
     privateekssubnetcidr=["10.0.3.0/24", "10.0.4.0/24"]
     privaterdssubnetcidr=["10.0.5.0/24", "10.0.6.0/24"]
     tags=local.defaulttags
     prefix="${local.defaulttags.project}-${local.defaulttags.environment}"
    
+}
+
+module "eks"{
+    source="./modules/eks"
+    prefix="${local.defaulttags.project}-${local.defaulttags.environment}"
+    eks_subnet_ids=module.vpc.eks_subnet_ids
+    min_size=1
+    desired_size=2
+    max_size=3
+    node_instance_type=["t3.small"]
+    tags=local.defaulttags
 }
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,3 +1,6 @@
 output "vpcid"{
     value=aws_vpc.main.id
 }
+output "eks_subnet_ids"{
+    value=aws_subnet.privateeks[*].id
+}
