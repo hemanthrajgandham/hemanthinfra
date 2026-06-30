dddfb3ee8d981270b413d09874e9eec7e2d1149f
hemanth <5hemanthunplugged@gmail.com>
2026-06-30 17:04:47 +0530

    update EKS outputs to reference the correct data source for endpoint and certificate authority

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -5,9 +5,9 @@ output "eks_cluster"{
   value=aws_eks_cluster.cluster.arn
 }
 output "endpoint" {
-  value = data.aws_eks_cluster.example.endpoint
+  value = data.aws_eks_cluster.cluster.endpoint
 }
 
 output "kubeconfig-certificate-authority-data" {
-  value = data.aws_eks_cluster.example.certificate_authority[0].data
+  value = data.aws_eks_cluster.cluster.certificate_authority[0].data
 }
